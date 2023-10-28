0d30642b3b74938bacf0670d14d34873636e3b9a
VladM <vdstories20@gmail.com>
2023-10-28 19:09:53 +0200

    make task

--- src/components/Redux/Contacts/Selectors.js
+++ src/components/Redux/Contacts/Selectors.js
@@ -1,4 +1,4 @@
-export const onGetContacts = state => state.contacts.contacts.items;
-export const onGetSearch = state => state;
+export const onGetContacts = state => state.contacts.items;
+export const onGetSearch = state => state.seacrh;
 export const selectError = state => state.contacts.error;
 export const selectSearch = state => state.selectSearch;
